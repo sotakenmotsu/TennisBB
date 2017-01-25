df833dffe840680c2f9be967703efc45befdfb5a
剱物蒼太 <sotakenmotsu@gmail.com>
2017-01-25 19:38:49 +0900

    pickerviewエラー解決

--- TennisBB/ContentsViewController.swift
+++ TennisBB/ContentsViewController.swift
@@ -16,7 +16,7 @@ class ContentsViewController: UIViewController, UIPickerViewDataSource, UIPicker
     
     @IBOutlet weak var MemberSelector: UITextField!
     var MpickerView: UIPickerView = UIPickerView()
-    let Mlist = ["", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",]
+    let Mlist = ["", "1人", "2人", "3人", "4人", "5人", "6人", "7人", "8人", "9人", "10人",]
     
     @IBOutlet weak var StartSelector: UITextField!
     var SpickerView: UIPickerView = UIPickerView()
@@ -101,10 +101,15 @@ class ContentsViewController: UIViewController, UIPickerViewDataSource, UIPicker
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        self.LevelSelector.text = Llist[row]
-        self.MemberSelector.text = Mlist[row]
-        self.StartSelector.text = Slist[row]
-        self.EndSelector.text = Elist[row]
+        if pickerView == LpickerView {
+            self.LevelSelector.text = Llist[row]
+        }else if pickerView == MpickerView {
+            self.MemberSelector.text = Mlist[row]
+        }else if pickerView == SpickerView {
+            self.StartSelector.text = Slist[row]
+        }else{
+            self.EndSelector.text = Elist[row]
+        }
     }
     
     func cancel() {
